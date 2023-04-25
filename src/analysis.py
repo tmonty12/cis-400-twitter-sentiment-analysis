b0cc15568c863eea9e942912709ed7475317d765
Gabriel Ruoff <geruoff@gmail.com>
2023-04-25 15:25:17 -0400

    Update top5words

--- src/analysis.py
+++ src/analysis.py
@@ -7,13 +7,17 @@ import pandas as pd
 from nltk.probability import FreqDist
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
+from nltk.tag.stanford import StanfordNERTagger
+
+import contractions
+
 
 class SentimentAnalysis:
     def __init__(self):
         self.tweets = None
 
     # load tweets from a json file
-    def _load(self, path="../data/tweets.json"):
+    def load_tweets_from_file(self, path="../data/tweets.json"):
         with open(path, "r") as f:
             data = json.loads(f.read())['data']
         self.tweets = pd.json_normalize(data)
@@ -26,12 +30,25 @@ class SentimentAnalysis:
 
     # return the top 5 words from a set of tweets
     def top5words(self):
+        contracts = [word.replace("'", '') for word in list(contractions.contractions_dict.keys())]
         # prepare tweets by stripping out links and punctuation
         self.tweets['cleantext'] = self.tweets['text'].apply(SentimentAnalysis.cleanText)
         tokens = []
         for tweet in self.tweets['cleantext'].values:
-            # filter out stopwords
-            tokens += [word for word in nltk.word_tokenize(tweet) if word not in stopwords.words('english')]
+            # filter out stopwords and contractions
+            tokens += [word for word in nltk.word_tokenize(tweet) if word.lower() not in
+                       stopwords.words('english') + contracts]
+        # filter out non-nouns
+        tokens = [word[0] for word in nltk.pos_tag(tokens) if word[1] in ['NN', 'NNS', 'NNP']]
+        # initialize NERTagger
+        st = StanfordNERTagger('data/stanford-ner/english.all.3class.distsim.crf.ser.gz',
+                               'data/stanford-ner/stanford-ner-4.2.0.jar')
+        # tag tokens
+        tags = st.tag(tokens)
+        # filter to tokens tagged with PERSON
+        tags = set([tag[0].lower() for tag in tags if tag[1] in ['PERSON']])
+        # remove from list
+        tokens = [word for word in tokens if word.lower() not in tags]
         # return top 5 words
         return [word[0] for word in FreqDist(tokens).most_common(5)]
 
@@ -42,7 +59,6 @@ class SentimentAnalysis:
         return self.tweets['reputationscore'].mean()
 
     # compute the reputation score for a tweet.
-    # Reputation score = (pos-neg)*comp
     # Score range [-1, 1]
     @staticmethod
     def _reputation_score(s):
@@ -50,13 +66,24 @@ class SentimentAnalysis:
         scores = analyzer.polarity_scores(s)
         return (scores['pos']-scores['neg'])*scores['compound']
 
+    @staticmethod
+    def map_score(score):
+        mn1, mx1 = -0.2, 0.2
+        mn2, mx2 = 1, 10
+        c = SentimentAnalysis.clamp(mn2, mx2)
+        return c((mx2 - mn2) * (score - mn1) / (mx1 - mn1) + mn2)
+
+    @staticmethod
+    def clamp(mn, mx):
+        return lambda value: max(min(value, mx), mn)
+
     # remove non-letters, links and "rt" notation from tweets
     @staticmethod
     def cleanText(s):
         # remove rt notation and links
-        s = str(s).lower().replace('rt ', '')
+        s = str(s).replace('rt ', '')
         s = re.sub(r'https?:\/\/\S+', '', s)
         s = re.sub(r'@\w{1,15}(?!\w)', '', s)
         # remove non-letters
-        s = re.sub(r'[^a-zA-Z\s]', '', s)
+        s = re.sub(r'[^\'a-zA-Z\s]', '', s)
         return s
